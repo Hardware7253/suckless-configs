06b883bf41fd907bea5776c0739178320e92ee99
oxnh <77563973+Hardware7253@users.noreply.github.com>
2023-02-06 10:32:25 +1300

    Fixed battery status. Gpu temp disabled by default

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -1,13 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Bat:", "acpi | cut -c 18-", 30, 0},
+	{"Bat:", "cat /sys/class/power_supply/BAT0/capacity | sed 's/$/%/'", 30, 0},
 
 	{"Vol:", "pamixer --get-volume | sed 's/$/%/'", 1, 0},
 
 	{"Cpu:", "sensors | grep -i 'id' | awk 'NR==1{print $1,$2,$3,$4}' | cut -c  16- ", 15, 0},
 
-	{"Gpu:", "sensors | grep -i 'edge' | cut -b 15,16,17,18,19,20,21,22 | cut -c  2-", 15, 0},
+	//{"Gpu:", "sensors | grep -i 'edge' | cut -b 15,16,17,18,19,20,21,22 | cut -c  2-", 15, 0},
 
 	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
