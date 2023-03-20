2ad853fec463dbc27c1fa46ca834017ba7b42c7f
oxnh <77563973+Hardware7253@users.noreply.github.com>
2023-03-20 17:13:46 +1300

    Changed font to Hack

--- dwm/config.h
+++ dwm/config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Ubuntu Mono:style=regular:pixelsize=20:antialias=true:autohint=true" };
-static const char dmenufont[]       = "Ubuntu Mono:style=regular:pixelsize=20:antialias=true:autohint=true";
+static const char *fonts[]          = { "Hack:style=regular:pixelsize=18:antialias=true:autohint=true" };
+static const char dmenufont[]       = "Hack:style=regular:pixelsize=18:antialias=true:autohint=true"
 
 #include "/home/oxnh/.cache/wal/colors-wal-dwm.h"
 
@@ -26,7 +26,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
--- st/config_hdpi.h
+++ st/config_hdpi.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Ubuntu Mono:style=regular:pixelsize=36:antialias=true:autohint=true";
+static char *font = "Hack:style=regular:pixelsize=36:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
