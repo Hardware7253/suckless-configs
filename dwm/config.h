4982f0c82f8e93d607ed8b8a8552e7dce139aadf
oxnh <77563973+Hardware7253@users.noreply.github.com>
2023-03-20 17:30:07 +1300

    Added semicolon

--- dwm/config.h
+++ dwm/config.h
@@ -6,7 +6,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Hack:style=regular:pixelsize=18:antialias=true:autohint=true" };
-static const char dmenufont[]       = "Hack:style=regular:pixelsize=18:antialias=true:autohint=true"
+static const char dmenufont[]       = "Hack:style=regular:pixelsize=18:antialias=true:autohint=true";
 
 #include "/home/oxnh/.cache/wal/colors-wal-dwm.h"
 
