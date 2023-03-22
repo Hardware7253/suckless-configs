7b62025d8038b9477344ba50f6e1190b335800d5
oxnh <77563973+Hardware7253@users.noreply.github.com>
2023-03-22 19:36:01 +1300

    Removed unnecessary binds
    Removed keybinds I don't use that just mess things up

--- dwm/config_hdpi.h
+++ dwm/config_hdpi.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Hack:style=regular:pixelsize=36:antialias=true:autohint=true" };
-static const char dmenufont[]       = "Hack:style=regular:pixelsize=36:antialias=true:autohint=true";
+static const char *fonts[]          = { "JetBrains Mono:style=regular:pixelsize=36:antialias=true:autohint=true" };
+static const char dmenufont[]       = "JetBrains Mono:style=regular:pixelsize=36:antialias=true:autohint=true";
 
 #include "/home/oxnh/.cache/wal/colors-wal-dwm.h"
 
@@ -59,18 +59,11 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,	                XK_q,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
